d6f9e79876c15f54e30792c21193915c97827f05
hiroyuki-azuma <azm1658120@gmail.com>
2023-04-06 01:35:46 +0900

    2023年4月6日

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -13,13 +13,25 @@
             <div class="px-10 mt-4">
 
             <!-- 編集ボタンの追加 -->
-            <a href="{{route('post.edit', $post)}}"><x-primary-button class="bg-teal-700 float-right">編集</x-primary-button></a>
+            <!-- <a href="{{route('post.edit', $post)}}"><x-primary-button class="bg-teal-700 float-right">編集</x-primary-button></a> -->
             
                 <div class="bg-white w-full  rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
                     <div class="mt-4">
-                        <h1 class="text-lg text-gray-700 font-semibold">
-                            {{ $post->title }}
-                        </h1><hr class="w-full">
+
+                            <h1 class="text-lg text-gray-700 font-semibold hover:underline cursor-pointer">
+                                {{ $post->title }}
+                            </h1>
+                            <hr class="w-full">
+                    </div>
+                    <div class="flex justify-end mt-4">
+                        <a href="{{route('post.edit', $post)}}"><x-primary-button class="bg-teal-700 float-right">編集</x-primary-button></a>
+                        <form method="post" action="{{route('post.destroy', $post)}}">
+                            @csrf
+                            @method('delete')
+                            <x-primary-button class="bg-red-700 float-right ml-4" onClick="return confirm('本当に削除しますか？');">削除</x-primary-button>
+                        </form>
+                    </div>
+
                         <p class="mt-4 text-gray-600 py-4">{{$post->body}}</p>
                         @if($post->image)
                             <div>
